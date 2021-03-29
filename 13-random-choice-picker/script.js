9c8e8df5bbf4267b7e4fd898a8001f6d735e6ef8
Matheus Schneider <matheus.ssm@gmail.com>
2021-03-29 18:28:08 -0300

    added logic to randomly select a tag

--- 13-random-choice-picker/script.js
+++ 13-random-choice-picker/script.js
@@ -5,6 +5,14 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+
+    randomSelect();
+  }
 });
 
 function createTags(input) {
@@ -19,6 +27,45 @@ function createTags(input) {
     const tagEl = document.createElement('span');
     tagEl.classList.add('tag');
     tagEl.innerText = tag;
+    console.log(tagEl);
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+  const highlightDuration = 100;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, highlightDuration);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * highlightDuration);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
